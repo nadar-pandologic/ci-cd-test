f57ac501da39fc8e2f692fabcb59048e00031c94
nadaralp <nadaralp16@gmail.com>
2021-10-28 15:24:17 +0300

    added ci_cd.yml updates

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-aws cloudformation deploy --template-file ci_cd.yml --stack-name ci-cd-n1adar --capabilities CAPABILITY_NAMED_IAM 
+aws cloudformation deploy --template-file ci_cd.yml --stack-name ci-cd-n2adar --capabilities CAPABILITY_NAMED_IAM \
+ --parameter-overrides ProjectName=n1adar-cicd-proj
