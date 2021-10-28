13808ed43b59b49c8f74d091dce3dc27998d5732
nadaralp <nadaralp16@gmail.com>
2021-10-28 16:54:00 +0300

    another thing

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-aws cloudformation deploy --template-file ci_cd.yml --stack-name ci-cd-n2adar --capabilities CAPABILITY_NAMED_IAM \
+aws cloudformation deploy --template-file ci_cd.yml --stack-name ci-cd-n5adar-new --capabilities CAPABILITY_NAMED_IAM \
  --parameter-overrides ProjectName=n1adar-cicd-proj
