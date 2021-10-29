6b8e9e125423357150410d641cfebd8d457c237e
nadaralp <nadaralp16@gmail.com>
2021-10-29 15:34:26 +0300

    starting to check final flow

--- cf-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-aws cloudformation deploy --template-file ci_cd.yml --stack-name ci-cd-n5adar-new --capabilities CAPABILITY_NAMED_IAM \
- --parameter-overrides ProjectName=nadar-test-cicd, GitHubRepo=ci-cd-test, GitHubOwner=nadar-pandologic
