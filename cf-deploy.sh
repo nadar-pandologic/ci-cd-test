704b5a7500fb7bb84b63e0ea95f2534ba69d6b62
nadaralp <nadaralp16@gmail.com>
2021-10-29 12:49:44 +0300

    added .

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 aws cloudformation deploy --template-file ci_cd.yml --stack-name ci-cd-n5adar-new --capabilities CAPABILITY_NAMED_IAM \
- --parameter-overrides ProjectName=n1adar-cicd-proj
+ --parameter-overrides ProjectName=nadar-test-cicd, GitHubRepo=ci-cd-test, GitHubOwner=nadar-pandologic
